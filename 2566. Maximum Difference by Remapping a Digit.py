2af37e60a5ac0bcbe0d720cda733127c5aca3576
pypymi <dr.mizoguchi@gmail.com>
2023-05-22 00:23:01 +0900

    test

--- 2566. Maximum Difference by Remapping a Digit.py
+++ 2566. Maximum Difference by Remapping a Digit.py
@@ -3,5 +3,3 @@ class Solution:
         max_v = max(map(int,[str(num).replace(i,"9") for i in str(num)]))
         min_v = min(map(int,[str(num).replace(i,"0") for i in str(num)]))
         return max_v - min_v
-
-        
