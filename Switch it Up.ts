4c0451809ada56dc91caaef94eecbadabf3547f6
pypymi <dr.mizoguchi@gmail.com>
2023-06-01 03:38:13 +0900

    test

---
+++ Switch it Up.ts
@@ -0,0 +1,36 @@
+export function switchItUp(intNumber:number):string {
+  let result:string = ""
+  switch(intNumber){
+      case 0:
+        result = "Zero"
+        break
+      case 1:
+        result= "One"
+        break
+      case 2:
+        result= "Two"
+        break
+      case 3:
+        result= "Three"
+        break
+      case 4:
+        result= "Four"
+        break
+      case 5:
+        result= "Five"
+        break
+      case 6:
+        result= "Six"
+        break
+      case 7:
+        result= "Seven"
+        break
+      case 8:
+        result= "Eight"
+        break
+      case 9:
+        result= "Nine"
+        break
+  }
+  return result
+}
