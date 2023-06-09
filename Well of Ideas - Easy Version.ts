4ad34b81cf45d763d1b5d5f6c3428519fb911a8b
pypymi <dr.mizoguchi@gmail.com>
2023-06-10 04:05:18 +0900

    test

---
+++ Well of Ideas - Easy Version.ts
@@ -0,0 +1,4 @@
+export function well(x: string[]): string{
+  let condition = x.filter(v => v=='good').length
+  return condition>2 ? "I smell a series!" : condition==1 || condition==2 ? 'Publish!' : 'Fail!'
+}
