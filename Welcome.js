9a370188fa6fecf6bfc96563cb95e7a5f8c27ada
pypymi <dr.mizoguchi@gmail.com>
2023-07-06 12:13:48 +0900

    test

---
+++ Welcome.js
@@ -0,0 +1,23 @@
+let object = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso'
+}
+
+function greet(language) {
+	return object[language] || 'Welcome'
+}
