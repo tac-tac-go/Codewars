2e6d091be7889cd3a00839baa7472f004b5edfbc
pypymi <dr.mizoguchi@gmail.com>
2023-08-04 07:15:42 +0900

    test

---
+++ Calculate BMI.php
@@ -0,0 +1,7 @@
+function bmi($weight, $height) {
+  $bmi = $weight/($height**2);
+  if($bmi<=18.5){return "Underweight";}
+  if($bmi<=25.0){return "Normal";}
+  if($bmi<=30.){return "Overweight";}
+  if($bmi>30){return "Obese";}
+}
