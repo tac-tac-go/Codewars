831abff6d53f999b97fcea91c1ae8b18182403b7
pypymi <dr.mizoguchi@gmail.com>
2023-08-19 05:49:12 +0900

    test

--- Sum of differences in array.js
+++ Sum of differences in array.js
@@ -1,3 +1,3 @@
 function sumOfDifferences(arr) {
-  return arr.sort((a,b) => b-a).slice(1).map((v,i) => arr[i]-v).reduce((a,b) => a+b)
+  return arr.sort((a,b) => b-a).slice(1).map((v,i) => arr[i]-v).reduce((a,b) => a+b,0)
 }
