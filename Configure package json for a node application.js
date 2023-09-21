95e51b82c9d69b962e64c06af3334e2f2c95f698
pypymi <dr.mizoguchi@gmail.com>
2023-09-21 10:07:37 +0900

    test

---
+++ Configure package json for a node application.js
@@ -0,0 +1,11 @@
+const configuration = {
+  "name": "image_gallery_js",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+};
