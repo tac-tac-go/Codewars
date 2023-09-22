5dbd6c2091a0d5769c8631570b9f4244f23e8cb4
pypymi <dr.mizoguchi@gmail.com>
2023-09-22 10:30:30 +0900

    test

---
+++ Grasshopper - Create the rooms.js
@@ -0,0 +1,6 @@
+// Add rooms here
+var rooms = {
+  "Room_1":{"name":"Hardik","description":"69","completed":"Royal Inn"},
+  "Room_2":{"name":"Hardik","description":"69","completed":"Royal Inn"},
+  "Room_3":{"name":"Hardik","description":"69","completed":"Royal Inn"},
+}
