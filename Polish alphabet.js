12d7253368904d3f5b39390754d307658543ab47
pypymi <dr.mizoguchi@gmail.com>
2023-10-01 12:20:21 +0900

    test

---
+++ Polish alphabet.js
@@ -0,0 +1,10 @@
+const expression   = /[ąćęłńóśźż]/gi;
+
+const replacements = {
+    'ą': 'a', 'ć': 'c', 'ę': 'e', 'ł': 'l', 'ń': 'n', 'ó': 'o', 'ś': 's', 'ź': 'z', 'ż': 'z',
+    'Ą': 'A', 'Ć': 'C', 'Ę': 'E', 'Ł': 'L', 'Ń': 'N', 'Ó': 'O', 'Ś': 'S', 'Ź': 'Z', 'Ż': 'Z'
+};
+
+function correctPolishLetters (text) {
+  return text.replace(expression, (letter) => replacements[letter]);
+}
