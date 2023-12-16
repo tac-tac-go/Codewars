7729e317503ed97a0e1cf4304cf928ff3e91f0bb
pypymi <dr.mizoguchi@gmail.com>
2023-12-17 08:15:07 +0900

    test

---
+++ Alan Partridge II - Apple Turnover.dart
@@ -0,0 +1,3 @@
+String apple(dynamic a) {
+  return int.parse(a.toString())*int.parse(a.toString()) > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox.";
+}
