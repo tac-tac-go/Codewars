137822e290fe2afdcb5099ceed09364382769438
pypymi <dr.mizoguchi@gmail.com>
2023-12-25 08:50:49 +0900

    test

---
+++ Get the Middle Character.dart
@@ -0,0 +1,3 @@
+String getMiddle(String s) {
+  return s.length%2==0 ? s.substring(s.length~/2-1,s.length~/2+1) : s.substring(s.length~/2,s.length~/2+1); 
+}
---
+++ You're a square.dart
@@ -0,0 +1,5 @@
+import 'dart:math';
+isSquare(n) { 
+  return sqrt(n)%1==0;
+}
+
