af42ce5282facb41a2ef30c63d4db0b2dc577fb2
pypymi <dr.mizoguchi@gmail.com>
2024-01-06 10:22:38 +0900

    test

--- Two to One.js
+++ Two to One.js
@@ -1,3 +1,5 @@
-function longest(s1, s2) {
+export const longest = (s1:string, s2:string): string => {
+  return [...new Set(s1+s2)].sort().join('')
+}function longest(s1, s2) {
   return Array.from(new Set(s1.concat(s2))).sort().join("")
 }
