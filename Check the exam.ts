fda5fa9ccb2b2f00d85a9b6d97eba9a296d29348
pypymi <dr.mizoguchi@gmail.com>
2024-01-28 03:46:08 +0900

    test

---
+++ Check the exam.ts
@@ -0,0 +1,13 @@
+export function checkExam(array1: string[], array2: string[]): number {
+  let count = 0;
+  for(let i=0;i<array1.length;i++){
+    if(array1[i] === array2[i]){
+      count+=4
+    }else if(array2[i]==""){
+      count+=0
+    }else{
+      count-=1
+    }
+  }
+  return Math.max(0,count)
+}
