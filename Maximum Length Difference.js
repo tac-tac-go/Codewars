ec03cbc16ac37d5a92cc7765269aa00dbc85f400
pypymi <dr.mizoguchi@gmail.com>
2024-01-27 12:25:39 +0900

    test

---
+++ Maximum Length Difference.js
@@ -0,0 +1,11 @@
+function mxdiflg(a1, a2) {
+    let max_result = -1;
+    for(let i of a1){
+      for(let j of a2){
+        if(max_result<Math.abs(i.length-j.length)){
+          max_result=Math.abs(i.length-j.length)
+        }
+      }
+    }
+   return max_result
+}
