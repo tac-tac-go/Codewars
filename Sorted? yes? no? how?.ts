e577b6a889130d673453d909e56538f75ee9b819
pypymi <dr.mizoguchi@gmail.com>
2024-02-03 13:49:25 +0900

    test

---
+++ Sorted? yes? no? how?.ts
@@ -0,0 +1,9 @@
+export function isSortedAndHow(array:number[]): string {
+  if(JSON.stringify([...array].sort((a,b) => a-b)) === JSON.stringify([...array])){
+    return "yes, ascending"
+  }else if(JSON.stringify([...array].sort((a,b) => b-a)) === JSON.stringify([...array])){
+    return "yes, descending"
+  }else{
+    return "no"
+  }
+}
