2f441a2618cc35e43e9827e442c128890ccbeaca
pypymi <dr.mizoguchi@gmail.com>
2024-02-08 12:04:55 +0900

    test

---
+++ Head, Tail, Init and Last.ts
@@ -0,0 +1,5 @@
+export const head = (x:Array<number>) => x[0];
+export const tail = (x:Array<number>) => x.slice(1)
+export const init = (x:Array<number>) => x.slice(0, -1)
+export const last = (x:Array<number>) => x.slice(-1)[0]
+
