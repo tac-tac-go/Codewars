a01f6756fc787de95656b011befc06285bc65d6c
pypymi <dr.mizoguchi@gmail.com>
2024-02-12 05:33:34 +0900

    test

---
+++ Maximum Product.js
@@ -0,0 +1,7 @@
+function adjacentElementsProduct(array) {
+  let result = [];
+  for(let i=0;i<array.length-1;i++){
+    result.push(array[i]*array[i+1])
+  }
+  return Math.max.apply(null,result)
+}
