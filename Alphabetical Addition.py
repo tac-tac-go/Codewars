28e7ddec36946bcdffc429309e2f318d35183c3a
pypymi <dr.mizoguchi@gmail.com>
2024-02-27 14:33:35 +0900

    test

--- Alphabetical Addition.py
+++ Alphabetical Addition.py
@@ -1,7 +1,4 @@
 def add_letters(*letters):
     letters_c = list(letters)+["z"]
     alpha = "zabcdefghijklmnopqrstuvwxy";
-    return alpha[(sum([alpha.index(i) for i in letters_c])%26)]function addLetters(...letters) {
-  let alpha = "abcdefghijklmnopqrstuvwxyz";
-  return alpha[((letters.map(i => alpha.indexOf(i)+1).reduce((a,b) => a+b,26)-1)%26)]
-}
+    return alpha[(sum([alpha.index(i) for i in letters_c])%26)]
