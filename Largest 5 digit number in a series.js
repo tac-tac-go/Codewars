24b1c134d8a42bdddb4bdd257e768d2e8a30c621
pypymi <dr.mizoguchi@gmail.com>
2024-03-15 08:12:51 +0900

    test

---
+++ Largest 5 digit number in a series.js
@@ -0,0 +1,10 @@
+function solution(digits) {
+  let answer = 0;
+  for (let i=0; i<digits.length; i++){
+    let number = digits.substr(i, 5); 
+    if(Number(number) > answer){ 
+      answer = Number(number);
+    }
+  }
+  return answer;
+}
