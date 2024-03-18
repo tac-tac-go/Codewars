66c65c1c6322e7f520ef7212e452dd91d87a0113
pypymi <dr.mizoguchi@gmail.com>
2024-03-19 08:34:22 +0900

    test

---
+++ Collatz Conjecture Length.js
@@ -0,0 +1,13 @@
+function collatz(n) {
+  let result = [n];
+  while(n!=1){
+    if(n%2==0){
+      n = n/2
+      result.push(n);
+    }else{
+      n = n*3+1
+      result.push(n)
+    }
+  }
+  return result.length;
+}
