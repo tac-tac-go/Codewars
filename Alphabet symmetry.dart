6482f441c1b6bf8f07f99939dfcc14190c76e00f
pypymi <dr.mizoguchi@gmail.com>
2024-04-04 09:00:03 +0900

    test

---
+++ Alphabet symmetry.dart
@@ -0,0 +1,14 @@
+List<int> solve(List<String> arr) {
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  List<int> result = [];
+  for(var i=0;i<arr.length;i++){
+    var count = 0;
+    for(var j=0;j<arr[i].length;j++){
+      if(alpha.split("").indexOf(arr[i][j].toLowerCase())==j){
+        count +=1;
+      }
+    }
+    result.add(count);
+  }
+  return result;
+}
