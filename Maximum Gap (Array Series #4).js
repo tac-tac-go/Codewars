21932c31a5e7d0b45527bb7661a03f2460baca85
pypymi <dr.mizoguchi@gmail.com>
2024-04-20 10:46:51 +0900

    test

---
+++ Maximum Gap (Array Series #4).js
@@ -0,0 +1,10 @@
+function maxGap (numbers){
+  numbers.sort((a,b) => a-b)
+  let max = -1
+  for(let i=1;i<numbers.length;i++){
+    if(numbers[i]-numbers[i-1]>max){
+      max = numbers[i]-numbers[i-1]
+    }
+  }
+  return max
+}
