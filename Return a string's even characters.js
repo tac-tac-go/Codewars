43ed5c569b26f3b20701259b489637073426bc98
pypymi <dr.mizoguchi@gmail.com>
2024-04-29 10:35:11 +0900

    test

--- Return a string's even characters.js
+++ Return a string's even characters.js
@@ -1,4 +1,4 @@
 function evenChars(string) {
   if(string.length<2 || string.length>100)return "invalid string"
-  return string.split("").map((e,i) => (i+1)%2==0 ? e : "").filter(i => i)
+  return string.split("").filter((x,i) => i % 2)
 }
