385eb32d2024dd09c7b2a29267e5deb368b685f0
pypymi <dr.mizoguchi@gmail.com>
2024-04-30 09:09:00 +0900

    test

---
+++ Going to the cinema.dart
@@ -0,0 +1,10 @@
+int movie(int card, int ticket, double perc) {
+  var counter = 0;
+  num dticket = ticket;
+  num sum = card;
+  while (ticket * counter <= sum.ceil()) {
+    counter++;
+    sum += dticket *= perc;
+  }
+  return counter;
+}
