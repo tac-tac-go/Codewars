5e60ef8d6b589af4527ce8b0946e8179e952275f
pypymi <dr.mizoguchi@gmail.com>
2024-05-18 11:15:52 +0900

    test

--- String Merge.py
+++ String Merge.py
@@ -1,14 +1,2 @@
 def string_merge(string1, string2, letter):
-    result1 = ""
-    result2 = ""
-    for i1,v1 in enumerate(string1):
-        if letter==v1:
-            result1 = string1[:i1]
-            break
-            
-    for i2,v2 in enumerate(string2):
-        if letter==v2:
-            result2 = string2[i2:]
-            break        
-    return result1+result2
-    
+    return string1[:string1.index(letter)] + string2[string2.index(letter):]
