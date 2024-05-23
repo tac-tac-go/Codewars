b4bfec7cb9e27a078cb345e52e5996407741ed5d
pypymi <dr.mizoguchi@gmail.com>
2024-05-23 10:34:16 +0900

    test

---
+++ Word values.js
@@ -0,0 +1,14 @@
+function wordValue(a) {
+  const alpha = "abcdefghijklmnopqrstuvwxyz";
+  let result = [];
+  for(const [index,element1] of a.entries()){
+    let tmp = [];
+    for(const element2 of element1){
+       if(alpha.indexOf(element2)!=-1){
+         tmp.push((alpha.indexOf(element2)+1)*(index+1))
+       }
+    }
+    result.push(tmp.reduce((a,b) => a+b,0))
+  }
+  return result
+}
