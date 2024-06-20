7ecd459221f31cd26b639ef1eec104db043e1b67
pypymi <dr.mizoguchi@gmail.com>
2024-06-20 11:17:51 +0900

    test

---
+++ Balanced Number (Special Numbers Series #1 ).ts
@@ -0,0 +1,17 @@
+export function balancedNum(number: number): string {
+  let num = number.toString();
+  if (num.length <= 2) {
+    return "Balanced";
+  } else {
+    if (num.length % 2 === 1) {
+      let leftSum = num.slice(0, Math.floor(num.length / 2)).split('').reduce((acc, curr) => acc + parseInt(curr), 0);
+      let rightSum = num.slice(Math.floor(num.length / 2) + 1).split('').reduce((acc, curr) => acc + parseInt(curr), 0);
+      return leftSum === rightSum ? "Balanced" : "Not Balanced";
+    } else {
+      let leftSum = num.slice(0, Math.floor(num.length / 2) - 1).split('').reduce((acc, curr) => acc + parseInt(curr), 0);
+      let rightSum = num.slice(Math.floor(num.length / 2) + 1).split('').reduce((acc, curr) => acc + parseInt(curr), 0);
+      return leftSum === rightSum ? "Balanced" : "Not Balanced";
+    }
+  }
+}
+
