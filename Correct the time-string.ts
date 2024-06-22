47388bcd8b94c25430ebacbc1043c98abf98ac5a
pypymi <dr.mizoguchi@gmail.com>
2024-06-22 10:18:26 +0900

    test

---
+++ Correct the time-string.ts
@@ -0,0 +1,21 @@
+const divmod = (x:number, y:number) => [Math.floor(x / y), x % y];
+
+export function timeCorrect(timestring: string | null): string | null {
+  if(timestring === ""){
+    return ""
+  }else if(timestring === null){
+    return null
+  }else{
+    if(timestring.match(/[0-9]{2}:[0-9]{2}:[0-9]{2}/)){
+      let [h,m,s] = timestring.split(":")
+      const [s1,s2] = divmod(parseInt(s),60)
+      const [m1,m2] = divmod(s1+parseInt(m),60)
+      const [h1,h2] = divmod(m1+parseInt(h),24)      
+      return `${h2.toString().padStart(2, '0')}:${m2.toString().padStart( 2, '0')}:${s2.toString().padStart( 2, '0')}`
+    }else{
+      return null
+    }
+  }
+}
+
+
