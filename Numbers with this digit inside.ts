a63346c15c3c0f0f47c27d411ac35ef21897b6fa
pypymi <dr.mizoguchi@gmail.com>
2024-07-03 10:59:29 +0900

    test

---
+++ Numbers with this digit inside.ts
@@ -0,0 +1,4 @@
+export function numbersWithDigitInside(x:number, d:number):number[] {
+  let result = [...Array(x).keys()].map(i => i+1).filter(v => String(v).includes(String(d)))
+  return [result.length,result.reduce((a,b) => a+b,0),result.length===0 ? 0 : result.reduce((a,b) => a*b,1)]
+}
