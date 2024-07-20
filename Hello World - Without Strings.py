ce0aeab63b58f9bb0cfa2303fc0910026354938b
pypymi <dr.mizoguchi@gmail.com>
2024-07-20 10:47:40 +0900

    test

--- Hello World - Without Strings.py
+++ Hello World - Without Strings.py
@@ -1,3 +1,1 @@
-const helloWorld = () => {
-  return String.fromCharCode(72,101,108,108,111,44,32,87,111,114,108,100,33)
-};
+hello_world=lambda:bytes([72,101,108,108,111,44,32,87,111,114,108,100,33]).decode()
