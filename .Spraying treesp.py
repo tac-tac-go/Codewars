322692d0bcb1bb81620fc597af1370e8c78b72dc
pypymi <dr.mizoguchi@gmail.com>
2024-07-26 07:42:28 +0900

    test

---
+++ .Spraying treesp.py
@@ -0,0 +1,3 @@
+def task(w, n, c):
+    person = {'Monday':'James','Tuesday':'John','Wednesday':'Robert','Thursday':'Michael','Friday':'William'}
+    return f"It is {w} today, {person[w]}, you have to work, you must spray {n} trees and you need {n*c} dollars to buy liquid"
