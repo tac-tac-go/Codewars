7d3a90b6ae0e41bc2de268cb19d8b381f5c2d907
pypymi <dr.mizoguchi@gmail.com>
2024-08-05 09:59:14 +0900

    test

---
+++ Debug Basic Calculator.js
@@ -0,0 +1,13 @@
+function calculate(a, o, b) {
+    if (o === "+") {
+        return a + b;
+    } else if (o === "-") {
+        return a - b;
+    } else if (o === "/" && b !== 0) {
+        return a / b;
+    } else if (o === "*") {
+        return a * b;
+    }
+    return null;
+}
+
