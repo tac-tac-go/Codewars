b43a7e454578ba8f5a27e9b79dc66f7adfdb4e19
pypymi <dr.mizoguchi@gmail.com>
2024-08-09 19:07:49 +0900

    test

---
+++ Especially Joyful Numbers.js
@@ -0,0 +1,6 @@
+function numberJoy(n) {
+    harshad = String(n).split("").map(v => parseInt(v)).reduce((a,b) => a+b,0)
+    const condition1 = n%harshad==0
+    const condition2 = parseInt(String(harshad).split("").reverse().join(""))*harshad==n
+    return condition1 && condition2
+}
