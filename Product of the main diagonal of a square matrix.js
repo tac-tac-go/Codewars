b9110a2e774b6c3774514bbc615d68641629f2df
pypymi <dr.mizoguchi@gmail.com>
2024-08-15 10:59:47 +0900

    test

---
+++ Product of the main diagonal of a square matrix.js
@@ -0,0 +1,11 @@
+function mainDiagonalProduct(mat){
+  let result = 1;
+  for(let i=0;i<mat.length;i++){
+    for(let j=0;j<mat.length;j++){
+      if(i==j){
+        result*=mat[i][j]
+      }
+    }
+  }
+  return result
+}
