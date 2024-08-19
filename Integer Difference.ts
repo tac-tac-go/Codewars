3c07ed991e42d6b0cc34648add62b766dbbff3f3
pypymi <dr.mizoguchi@gmail.com>
2024-08-19 10:06:45 +0900

    test

---
+++ Integer Difference.ts
@@ -0,0 +1,11 @@
+export const intDiff = (arr: number[], n: number): number => {
+  let count = 0;
+  for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+      if(Math.abs(arr[i]-arr[j])===n){
+        count+=1
+      }
+    }
+  }
+  return count;
+}
