249baa49ff6eea409b0bef2e4d532a793ea036c5
pypymi <dr.mizoguchi@gmail.com>
2024-08-31 07:04:14 +0900

    test

--- Suzuki needs help lining up his students.js
+++ Suzuki needs help lining up his students.js
@@ -1,12 +1,3 @@
-const funcCompare = (a,b) => {
-  if (a[0] < b[0]) return 1;
-  if (a[0] > b[0]) return -1;
-  if (a[1] < b[1]) return 1;
-  if (a[1] > b[1]) return -1;
-  return 0;
-}
-
 function lineupStudents(students){
-  let arr = students.split(" ").map(v => [v.length,v])
-  return [...arr].sort(funcCompare).map(v => v[1])
+  return students.split(" ").sort((a,b) => b.length - a.length || b.localeCompare(a))
 }
