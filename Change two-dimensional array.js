c397ba4d6588ca4f847627989334b6aaf1d0e8b6
pypymi <dr.mizoguchi@gmail.com>
2024-09-02 11:23:43 +0900

    test

--- Change two-dimensional array.js
+++ Change two-dimensional array.js
@@ -1,14 +1,6 @@
 function matrix(array) {
-  for(let i=0;i<array.length;i++){
-    for(let j=0;j<array.length;j++){
-      if(i==j){
-        if(array[i][j]<0){
-          array[i][j]=0
-        }else{
-          array[i][j]=1
-        }
-      }
+    for (let i=0; i<array.length; i++) {
+      array[i][i] = array[i][i] < 0 ? 0 : 1
     }
-  }
-  return array
-} 
+    return array;
+}
