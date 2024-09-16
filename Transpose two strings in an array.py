0ea59c8573666b62610edcc4c5004dd3b24fd262
pypymi <dr.mizoguchi@gmail.com>
2024-09-16 10:46:12 +0900

    test

--- Transpose two strings in an array.py
+++ Transpose two strings in an array.py
@@ -1,18 +1,3 @@
 from itertools import zip_longest
 def transpose_two_strings(arr):
-    result = []
-    for i,j in zip_longest(arr[0],arr[1]):
-        tmp = ""
-        if i is None:
-            tmp+=" "
-        else:
-            tmp+=i
-        tmp+=" "
-        if j is None:
-            tmp+=" "
-        else:
-            tmp+=j
-        result.append(tmp)
-    return "\n".join(result)
-        
-        
+    return "\n".join(f"{a} {b}" for a, b in zip_longest(*arr, fillvalue=" "))
