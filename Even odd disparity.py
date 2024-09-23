ef55988db4266523c2b8e7a4725dc182d111f016
pypymi <dr.mizoguchi@gmail.com>
2024-09-23 13:36:01 +0900

    test

--- Even odd disparity.py
+++ Even odd disparity.py
@@ -1,10 +1,2 @@
 def solve(a):
-    even = 0
-    odd = 0
-    for i in a:
-        if isinstance(i,int):
-            if int(i)%2==0:
-                even+=1
-            else:
-                odd+=1
-    return even-odd
+    return sum(1 if int(i)%2==0 else -1 for i in a if isinstance(i,int))
