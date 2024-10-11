20a545b57f5b7065c98cfa61b855caccfc817215
pypymi <dr.mizoguchi@gmail.com>
2024-10-11 12:09:58 +0900

    test

---
+++ Safen User Input Part I - htmlspecialchars.ts
@@ -0,0 +1,6 @@
+export function htmlspecialchars(formData: string): string {
+  return formData.replace(/&/g, "&amp;")
+                 .replace(/"/g, "&quot;")
+                 .replace(/</g, "&lt;")
+                 .replace(/>/g, "&gt;");
+}
