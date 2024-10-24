bf795adc10992ad0e56782fbbc0e844aec74253d
pypymi <11955262+tac-tac-go@users.noreply.github.com>
2024-10-24 10:22:40 +0900

    test

--- Jumping Number (Special Numbers Series #4).ts
+++ Jumping Number (Special Numbers Series #4).ts
@@ -1,7 +1,7 @@
 export function jumpingNumber(n: number) {
   const arr = [...String(n)];
   for(let i=0;i<arr.length-1;i++){
-    if(Math.abs(parseInt(arr[i+1])-parseInt(arr[i]))>1){
+    if(Math.abs(parseInt(arr[i+1])-parseInt(arr[i]))!==1){
       return "Not!!"
     }
   }
