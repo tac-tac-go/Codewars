a710f652515fc3eabcfff4208dd90f97fd7e6e1d
pypymi <dr.mizoguchi@gmail.com>
2024-11-15 11:51:36 +0900

    test

---
+++ Build Tower.ts
@@ -0,0 +1,9 @@
+export const towerBuilder = (nFloors: number): string[] => {
+  let tower = [];
+  for (let i = 0; i < nFloors; i++) {
+    tower.push(" ".repeat(nFloors - i - 1)
+             + "*".repeat((i * 2)+ 1)
+             + " ".repeat(nFloors - i - 1));
+  }
+  return tower;
+}
