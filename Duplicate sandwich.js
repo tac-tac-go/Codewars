edb15db015993035f2647a0e9231efdbc4eaf7ef
pypymi <dr.mizoguchi@gmail.com>
2024-12-24 08:55:04 +0900

    test

--- Duplicate sandwich.js
+++ Duplicate sandwich.js
@@ -1,10 +1,4 @@
-function duplicateSandwich(a) {
-  let count = {}
-  for(let i=0;i<a.length;i++){
-    const val = a[i]
-    count[val] = (count[val] || 0)+1
-    if(count[val]==2){
-      return a.slice(a.indexOf(a[i])+1,a.lastIndexOf(a[i]))
-    }
-  }
+const duplicateSandwich = (list) => {
+  let duplicated = [...list].find(a => list.indexOf(a) !== list.lastIndexOf(a))
+  return list.slice(list.indexOf(duplicated) + 1, list.lastIndexOf(duplicated))
 }
