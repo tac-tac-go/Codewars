e6aa871f60c1624793b966ac91e12e099e53c459
pypymi <dr.mizoguchi@gmail.com>
2025-01-01 01:31:23 +0900

    test

---
+++ Human Readable Time.js
@@ -0,0 +1,7 @@
+function humanReadable(seconds) {
+  const hour = Math.floor(seconds / 3600)
+  seconds = seconds % 3600
+  const minutes = Math.floor(seconds / 60)
+  seconds = seconds%60
+  return String(hour).padStart(2, '0')+":"+String(minutes).padStart(2, '0')+":"+String(seconds).padStart(2, '0')
+}
