70a7e2c03a57105ebf4dcfb204c54801a850de31
pypymi <dr.mizoguchi@gmail.com>
2025-02-12 13:21:11 +0900

    test

--- Initialize my name.py
+++ Initialize my name.py
@@ -1,8 +1,11 @@
-def initialize_names(name):
-    name_a = name.split(" ")
-    if len(name_a)==1:
-        return name_a[0]
-    if len(name_a)==2:
-        return " ".join(name_a)
-    return " ".join([name_a[0]," ".join(list(map(lambda x:x[0]+".",name_a[1:-1]))),name_a[-1]])
-
+const initializeNames = (name) => {
+  let arr = name.split(" ")
+  if(arr.length<=2){
+    return name
+  }else{
+    const f = arr.slice(0,1)[0]
+    const l = arr.slice(arr.length-1,)[0]
+    let m = arr.slice(1,arr.length-1).map(v => v[0]+".").join(" ")
+    return `${f} ${m} ${l}`
+  }
+}
