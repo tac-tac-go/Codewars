e715cc6dce45f9ab2ad5f6d3cc87f55a25674355
pypymi <dr.mizoguchi@gmail.com>
2025-03-07 11:37:13 +0900

    test

--- Simple Fun #40: Timed Reading.js
+++ Simple Fun #40: Timed Reading.js
@@ -1,3 +1,4 @@
 function timedReading(maxLength, text) {
-  return text.replace(/[^a-z\s]/ig,"").split(" ").filter(v => v.length <= maxLength && v.length > 0).length
+  const words = text.match(/[a-zA-Z]+/g) || [];
+  return words.filter(v => v.length <= maxLength).length
 }
