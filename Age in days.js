603feefcb93be8ee86e708c55d35c881d151da1a
pypymi <dr.mizoguchi@gmail.com>
2025-03-18 01:01:38 +0900

    test

--- Age in days.js
+++ Age in days.js
@@ -1,6 +1,5 @@
 function ageInDays(year, month, day){
   const now = new Date();
-  let one = 24*60*60*1000;
   const birth = new Date(year,month-1,day)
-  return `You are ${Math.floor((now-birth)/one)} days old`
+  return `You are ${Math.floor((now-birth)/(24*60*60*1000))} days old`
 }
