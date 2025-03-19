c13b7368c54b0239ea4c3e5ecf365178bb8c11dc
pypymi <dr.mizoguchi@gmail.com>
2025-03-20 02:42:43 +0900

    test

--- Alan Partridge III - London.js
+++ Alan Partridge III - London.js
@@ -1,7 +1,5 @@
 function alan(x){
-  const fil1 = x.filter(v => v==='Rejection').length>=1
-  const fil2 = x.filter(v => v==='Disappointment').length>=1
-  const fil3 = x.filter(v => v==='Backstabbing Central').length>=1
-  const fil4 = x.filter(v => v==='Shattered Dreams Parkway').length>=1
-  return fil1 && fil2 && fil3 && fil4 ? "Smell my cheese you mother!" : "No, seriously, run. You will miss it."
+  return ['Rejection', 'Disappointment', 'Backstabbing Central', 'Shattered Dreams Parkway'].every(e => x.indexOf(e) != -1) 
+  ? 'Smell my cheese you mother!' 
+  : 'No, seriously, run. You will miss it.';  
 }
