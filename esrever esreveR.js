c78633ecfb75d4ebcf75da9c50d42909dcbbdf7c
pypymi <dr.mizoguchi@gmail.com>
2025-03-26 10:17:40 +0900

    test

--- esrever esreveR.js
+++ esrever esreveR.js
@@ -1,4 +1,1 @@
-function esrever( str ){
-  if(str.length===0)return ""
-  return [...str].slice(0,str.length-1).reverse().join("")+str.slice(str.length-1,str.length)
-}
+const esrever =(s) => s.slice(0,-1).split("").reverse().join("")+s.slice(-1)
