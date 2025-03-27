7da13ee9d617778d126e70314d5cced159fbbf66
pypymi <dr.mizoguchi@gmail.com>
2025-03-28 00:57:04 +0900

    test

---
+++ Difference between biggest 2 numbers.js
@@ -0,0 +1,14 @@
+function diffBig2(arr) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (const num of arr) {
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    } else if (num > secondLargest) {
+      secondLargest = num;
+    }
+  }
+  return largest-secondLargest
+}
