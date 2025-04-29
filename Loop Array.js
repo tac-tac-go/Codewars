5e8fddeee8bb48df4bc0ebdb642e07f7d701fea5
pypymi <dr.mizoguchi@gmail.com>
2025-04-29 13:24:56 +0900

    test

---
+++ Loop Array.js
@@ -0,0 +1,12 @@
+function loopArr(arr, direction, steps) {
+  for (let i = 0; i < steps; i++) {
+    if (direction === 'right'){
+      arr.unshift(arr.pop());
+    };
+    if (direction === 'left') {
+       arr.push(arr.shift());
+    };
+  }
+  return arr;
+}
+
