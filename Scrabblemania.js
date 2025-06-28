dd08532b4d0cdf79abbadc80b73220ea1efcf099
pypymi <dr.mizoguchi@gmail.com>
2025-06-28 11:07:56 +0900

    test

---
+++ Scrabblemania.js
@@ -0,0 +1,5 @@
+function wordscore(word) {
+  const player = {"a" : 1, "b" : 3,"c" : 3,"d" : 2, "e" : 1, "f" : 4,"g" : 2,"h" : 4,"i" : 1,"j" : 8,"k" : 5,"l" : 1,"m" : 3,"n" : 1,"o" : 1,"p" : 3,"q" : 10,"r" : 1,"s" : 1,"t" : 1,"u" : 1,"v" : 4, "w" : 4,"x" : 8,"y" : 4,"z" : 10}
+  const score = [...word].map(v => player[v]).reduce((a,b) => a+b,0)*word.length
+  return word.length===7 ? score+50 : score
+}
