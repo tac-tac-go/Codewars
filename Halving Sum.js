1e8a9d8d2388976663c8cac2e9f437d3e1e320a6
pypymi <dr.mizoguchi@gmail.com>
2025-07-27 08:19:06 +0900

    test

--- Halving Sum.js
+++ Halving Sum.js
@@ -1,10 +1,4 @@
 function halvingSum(n) {
-  let index = 1
-  let result = 0
-  while(Math.floor(n/index)>=1){
-    result+=Math.floor(n/index)
-    index*=2
-  }
-  return result
+    if(n === 1) return n;
+    return n + halvingSum(Math.floor(n/2));
 }
-
