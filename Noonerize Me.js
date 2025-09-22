7c785ddbc7e49c76fa950c9514dcb0dc69dc764f
pypymi <dr.mizoguchi@gmail.com>
2025-09-22 17:47:45 +0900

    test

---
+++ Noonerize Me.js
@@ -0,0 +1,6 @@
+function noonerize(numbers) {
+  let x, y;
+  [x, y] = numbers.map(String);
+  [x, y] = [y[0] + x.slice(1), x[0] + y.slice(1)].map(Number);
+  return x && y ? Math.abs(x - y) : 'invalid array';
+}
