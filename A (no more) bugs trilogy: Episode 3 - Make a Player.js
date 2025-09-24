01875409dfe1fac95a410a530f37b4076c1d570f
pypymi <dr.mizoguchi@gmail.com>
2025-09-24 18:22:16 +0900

    test

---
+++ A (no more) bugs trilogy: Episode 3 - Make a Player.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor(name, position, age, dribbling, pass, shoot) {
+    this.name = name
+    this.position = position
+    this.age = age
+    this.dribbling = dribbling
+    this.pass = pass
+    this.shoot = shoot
+  }
+}
