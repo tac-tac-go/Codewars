f88ef5ca2a5c220a7b1842e404b38cd6d14e64d4
pypymi <dr.mizoguchi@gmail.com>
2025-09-25 10:34:14 +0900

    test

--- Complete The Pattern #2.js
+++ Complete The Pattern #2.js
@@ -1,7 +1,2 @@
-function pattern(n){
- let tmp1 = []
- for(let i=0;i<n;i++){
-   tmp1.push(Array.from({ length: n-i }, (_, i) => String(n-i)).join(""))
- }
- return tmp1.join("\n")
-}
+const pattern = n =>
+                Array.from({length: n}, (_, i) => Array.from({length: n - i}, (_, j) => n - j).join('')).join('\n');
