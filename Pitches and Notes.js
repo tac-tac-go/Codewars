be0989e1b2f1413a2c58c4815c99de3aba4bd4b7
pypymi <dr.mizoguchi@gmail.com>
2025-10-01 09:24:34 +0900

    test

--- Pitches and Notes.js
+++ Pitches and Notes.js
@@ -1,1 +1,1 @@
-const getNote = (pitch) => Object.entries(notesDictionary).filter(([k,v]) => pitch >= Number(k) ? pitch%Number(k)===0 : Number(k)%pitch===0)[0][1]
+const getNote = (pitch) => Object.entries(notesDictionary).filter(([k,v]) => pitch%Number(k)===0 || Number(k)%pitch===0)[0][1]
