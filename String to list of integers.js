161a88fdb7abf344b1080def7a3e84aa3110f67d
pypymi <dr.mizoguchi@gmail.com>
2025-10-18 12:10:47 +0900

    test

--- String to list of integers.js
+++ String to list of integers.js
@@ -1,4 +1,1 @@
-function stringToIntArray(s){
-  const result  = s.replace(/,{2,}/g,",").replace(/,*$/, "").replace(/^,*/, "")
-  return result ? result.split(",").map(Number) : []
-}
+const stringToIntArray = (s) => s.split(',').filter(Boolean).map(Number)
