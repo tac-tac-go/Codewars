3c807726702a032208acc4605fb2a2d48a9b3647
pypymi <dr.mizoguchi@gmail.com>
2025-12-02 08:49:02 +0900

    test

--- noobCode 03: CHECK THESE LETTERS... see if letters in String 2 are present in String 1.js
+++ noobCode 03: CHECK THESE LETTERS... see if letters in String 2 are present in String 1.js
@@ -1,1 +1,1 @@
-const letterCheck = (arr) => [...arr[1].toLowerCase()].every(v => arr[0].toLowerCase().indexOf(v)!==-1)
+const letterCheck = (arr) => [...arr[1].toLowerCase()].every(v => arr[0].toLowerCase().includes(v))
