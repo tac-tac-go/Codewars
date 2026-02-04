8a4b3053337e679ccd8ddf4da2fc732e46a3f02c
pypymi <dr.mizoguchi@gmail.com>
2026-02-04 11:12:38 +0900

    test

--- Simple string matching.js
+++ Simple string matching.js
@@ -1,9 +1,1 @@
-function solve(a,b){
-  if(!a.includes("*"))return a===b
-  else{
-    const index = a.indexOf("*")
-    const prefix = a.slice(0,index)
-    const suffix = a.slice(index+1,)
-    return b.startsWith(prefix) && b.endsWith(suffix) && (prefix.length+suffix.length)<=b.length
-  }  
-}
+const solve = (a, b) => new RegExp('^' + a.replace('*', '.*') + '$').test(b);
