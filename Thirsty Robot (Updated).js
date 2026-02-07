b1c9d6e10a16f5fe5f7b2dc896c45a01bc69e682
pypymi <dr.mizoguchi@gmail.com>
2026-02-07 11:42:17 +0900

    test

--- Thirsty Robot (Updated).js
+++ Thirsty Robot (Updated).js
@@ -1,16 +1,12 @@
 function thirstyRobot(arr){
-  let isCheckBeer = false;
-  let isCheckWine = false;
-  let isLiquor = false;
+  let [isBeer,isWine,isLiquor] = [false,false,false]
   for(let tmp of arr){
-    if(tmp > 15)isLiquor = true;
-    else if(tmp > 8)isCheckWine = true
-    else{
-      if(isCheckWine)isCheckBeer = true
-    }
+    if(isWine && tmp<=8)isBeer = true;
+    else if(tmp > 8 && tmp <=15 )isWine = true
+    if (tmp > 15) isLiquor = true;
   }
-  if(isCheckBeer && isLiquor)return "Poor robot"
-  else if(isCheckBeer)return "Oh dear"
-  else if(isLiquor)return "Wonky robot"
+  if(isBeer && isLiquor) return "Poor robot"
+  else if(isBeer) return "Oh dear"
+  else if(isLiquor) return "Wonky robot"
   else return "Fine"
 }
