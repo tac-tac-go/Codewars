b1415f76f8c3bff27841d23bdebba8cc35010fc1
pypymi <dr.mizoguchi@gmail.com>
2026-05-16 04:45:04 +0900

    2026-05-16

---
+++ Quarter of the year/Quarter of the year.swift
@@ -0,0 +1,6 @@
+func quarter(of month: Int) -> Int {
+  if month >= 1 && month <= 3 { return 1 }
+  if month >= 4 && month <= 6 { return 2 }
+  if month >= 7 && month <= 9 { return 3 }
+  return 4
+}
