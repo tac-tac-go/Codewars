f17f8b5dd5df6d519efd3d06b1e4d34f1ce1c227
pypymi <dr.mizoguchi@gmail.com>
2026-09-04 18:23:28 +0900

    2026-09-04

---
+++ Responsible Drinking/Responsible Drinking.ts
@@ -0,0 +1,4 @@
+export function hydrate(s: string): string {
+  const result = s.split(/[^0-9]/g).filter(v => String(v).length>0).map(Number).reduce((a,b) => a+b,0)
+  return result>1 ? `${result} glasses of water` : `${result} glass of water`
+}
