b93040b729a02b235534a6e50dd693e5943364b3
pypymi <dr.mizoguchi@gmail.com>
2026-09-08 11:52:30 +0900

    2026-09-08

--- Regexp Basics - is it a six bit unsigned number?/Regexp Basics - is it a six bit unsigned number?.ts
+++ Regexp Basics - is it a six bit unsigned number?/Regexp Basics - is it a six bit unsigned number?.ts
@@ -1,3 +1,3 @@
-String.prototype.sixBitNumber = function() {
-  return /^(\d|[1-5]\d|6[0-3])$/.test(this.toString());
+String.prototype.sixBitNumber = function(): boolean {
+  return /^([1-5]?\d|6[0-3])$/.test(String(this));
 }
